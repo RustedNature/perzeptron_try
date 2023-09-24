6834fbe05e85c34a69ceefc76013a63971c80a72
Nico <133128777+RustedNature@users.noreply.github.com>
2023-09-24 15:50:17 +0200

    test for size of weights and input_neurons

--- src/tests/perzeptron_tests.rs
+++ src/tests/perzeptron_tests.rs
@@ -1,6 +1,19 @@
 use crate::perzeptron::*;
-
+const INPUT_SIZE: u32 = 1920 * 1080;
 #[test]
 fn can_construct_perzeptron() {
-    let perz = Perzeptron::new(64*64);
+    let perz = Perzeptron::new(INPUT_SIZE);
+}
+
+#[test]
+fn correct_size_of_input_neurons() {
+    let perz = Perzeptron::new(INPUT_SIZE);
+    let num_of_input_neurons = perz.get_length_of_input_neurons_vec();
+    assert!(num_of_input_neurons == INPUT_SIZE as usize)
+}
+#[test]
+fn correct_size_of_weights() {
+    let perz = Perzeptron::new(INPUT_SIZE);
+    let num_of_weights = perz.get_length_of_weights_vec();
+    assert!(num_of_weights == INPUT_SIZE as usize)
 }
