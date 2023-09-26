c3567ecd3ab201f1474193247682de4518d4f084
Nico <133128777+RustedNature@users.noreply.github.com>
2023-09-26 17:44:42 +0200

    some calculation logic

--- src/perzeptron.rs
+++ src/perzeptron.rs
@@ -24,23 +24,53 @@ impl Perzeptron {
     pub fn get_length_of_weights_vec(&self) -> usize {
         self.weights.len()
     }
+
+    pub fn calculate_output(&mut self) {
+        let mut output_value: f64 = 0.0;
+        for (input_neuron_number, input_neuron) in self.input_neurons.iter().enumerate() {
+            output_value += input_neuron.get_input_value()
+                * self.weights[input_neuron_number].get_wight_factor();
+        }
+        self.output_neuron.evaluate_output(output_value);
+    }
+
+    pub fn get_output_neuron_value(&self) -> bool {
+        self.output_neuron.get_output()
+    }
 }
 
 struct InputNeuron {
-    value: i32,
+    value: f64,
 }
 impl InputNeuron {
     pub fn new() -> Self {
-        InputNeuron { value: 0 }
+        InputNeuron { value: 1.0 }
+    }
+    pub fn get_input_value(&self) -> f64 {
+        self.value
     }
 }
 
 struct OutputNeuron {
     value: bool,
+    theta: f64,
 }
 impl OutputNeuron {
     pub fn new() -> Self {
-        OutputNeuron { value: false }
+        OutputNeuron {
+            value: false,
+            theta: 1.0,
+        }
+    }
+
+    pub fn get_output(&self) -> bool {
+        self.value
+    }
+
+    pub fn evaluate_output(&mut self, value: f64) {
+        if value >= self.theta {
+            self.value = true;
+        }
     }
 }
 
@@ -49,6 +79,12 @@ struct Weight {
 }
 impl Weight {
     pub fn new() -> Self {
-        Weight { weight_factor: 0.0 }
+        Weight {
+            weight_factor: 0.5785,
+        }
+    }
+
+    pub fn get_wight_factor(&self) -> f64 {
+        self.weight_factor
     }
 }
--- src/tests/perzeptron_tests.rs
+++ src/tests/perzeptron_tests.rs
@@ -1,4 +1,4 @@
-use crate::perzeptron::*;
+use crate::perzeptron::{self, *};
 const INPUT_SIZE: u32 = 1920 * 1080;
 #[test]
 fn can_construct_perzeptron() {
@@ -16,4 +16,12 @@ fn correct_size_of_weights() {
     let perz = Perzeptron::new(INPUT_SIZE);
     let num_of_weights = perz.get_length_of_weights_vec();
     assert!(num_of_weights == INPUT_SIZE as usize)
+}
+
+#[test]
+fn get_expectect_output() {
+    let mut perzeptron = Perzeptron::new(INPUT_SIZE);
+    perzeptron.calculate_output();
+    let out = perzeptron.get_output_neuron_value() == true;
+    assert!(out)
 }
